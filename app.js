47279216a6ac9d09362e9d2d9d15d149afa1c127
Kyle Knickerbocker <knickerbocker.chris@gmail.com>
2019-10-30 06:47:41 -0500

    create projects view page and projects route handler

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
+// const projectsRouter = require('./routes/projects');
 
 const app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+// app.use('/projects', projectsRouter);
 app.use('/projects', projectsRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/projects', function (req, res, next) {
-    res.render('projects', { title: 'Kyle Knickerbocker' });
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
