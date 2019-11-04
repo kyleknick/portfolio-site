8555761921268693c0ce998dac73572300b625c9
Kyle Knickerbocker <knickerbocker.chris@gmail.com>
2019-11-04 06:43:25 -0600

    Misc updates

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const projectRouter = require('./routes/projects');
 const articleRouter = require('./routes/articles');
-const aboutRouter = require('./routes/articles');
+const aboutRouter = require('./routes/about');
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/projects', projectRouter);
 app.use('/articles', articleRouter);
-app.use('about', aboutRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
